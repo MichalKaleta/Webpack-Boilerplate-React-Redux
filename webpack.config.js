3bb868776a1f2d851fb532d84739d18e6e055c9d
Michał Kaleta <szefwszechswiata@gmail.com>
2018-02-19 13:15:01 +0100

    first working version

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,21 @@
-const path = require('path');
+//const path = require('path');
 
+module.exports= {
+  entry: "./src/index.js",
+  
+  output: {
+  filename: './bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['env', 'react']
+        }
+      }
+    ]
+  }
+}
